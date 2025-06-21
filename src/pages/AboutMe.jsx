e45e1c0d441bf69a3b589b137291e822ffaafc48
valeru19 <lilkogin01@gmail.com>
2025-06-21 16:51:21 +0500

    upgrade About me and colors2

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -17,16 +17,16 @@ const containerVariants = {
         width: '100%',
         transition: { duration: 0.5, ease: 'easeInOut' },
     },
-    };
+};
 
     // Варианты анимации для заголовка
-    const titleVariants = {
+const titleVariants = {
     initial: { y: 0 },
     shifted: { y: -20, transition: { duration: 0.5, ease: 'easeInOut' } },
-    };
+};
 
     // Варианты анимации для изображений
-    const imageVariants = {
+const imageVariants = {
     hidden: {
         opacity: 0,
         x: '100vw',
@@ -34,16 +34,26 @@ const containerVariants = {
     visible: {
         opacity: 1,
         x: 0,
-        rotate: 360,
+        rotate: -360,
         transition: {
-        x: { duration: 1, ease: 'easeOut' },
+        x: { duration: 2, ease: 'linear' },
         rotate: { repeat: Infinity, duration: 3, ease: 'linear' },
         },
     },
-    };
+
+    exit: {
+        opacity: 0,
+        x: '-100vw',
+        rotate: -360,
+        transition: {
+        x: { duration: 2, ease: 'linear' },
+        rotate: { repeat: Infinity, duration: 3, ease: 'linear' },
+        },
+    },
+};
 
     // Варианты анимации для текста
-    const textVariants = {
+const textVariants = {
     hidden: {
         opacity: 0,
         y: 50,
@@ -57,10 +67,10 @@ const containerVariants = {
         delay: 0.5, // Задержка для появления после изображения
         },
     },
-    };
+};
 
     // Компонент для страницы "About me"
-    const About = () => {
+const About = () => {
     // Состояние для управления видимостью контента в каждом контейнере
     const [isExpanded, setIsExpanded] = useState({
         container1: false,
@@ -84,29 +94,29 @@ const containerVariants = {
                 color: 'var(--text-color)',
                 padding: '50px 0',
             }}
-            >
+        >
             <div className="container">
                 {/* Контейнер 1 */}
                 <motion.div
-                className="about-container"
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                viewport={{ once: true, amount: 0.3 }}
-                variants={containerVariants}
-                animate={isExpanded.container1 ? 'expanded' : 'collapsed'}
-                style={{
-                    marginBottom: '50px',
-                    textAlign: 'center',
-                    backgroundColor: '#6ad4ad',
-                    padding: '20px',
-                    borderRadius: '10px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    maxWidth: '1200px',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                }}
+                    className="about-container"
+                    initial={{ opacity: 0, scale: 0 }}
+                    whileInView={{ opacity: 1, scale: 1 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    variants={containerVariants}
+                    animate={isExpanded.container1 ? 'expanded' : 'collapsed'}
+                    style={{
+                        marginBottom: '50px',
+                        textAlign: 'center',
+                        backgroundColor: '#1ed894',
+                        padding: '20px',
+                        borderRadius: '10px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        maxWidth: '1200px',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                    }}
                 >
                 <motion.h2
                     className="title-1"
@@ -117,65 +127,66 @@ const containerVariants = {
                 >
                     Who Am I?
                 </motion.h2>
-                <AnimatePresence>
-                    {isExpanded.container1 && (
-                    <>
-                        <motion.img
-                        src={about1}
-                        alt="About Me 1"
-                        style={{
-                            width: '450px',
-                            height: '400px',
-                            objectFit: 'cover',
-                            borderRadius: '10px',
-                            margin: '20px 0',
-                        }}
-                        variants={imageVariants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                        />
-                        <motion.p
-                        style={{
-                            maxWidth: '600px',
-                            margin: '20px auto',
-                            fontSize: '25px',
-                        }}
-                        variants={textVariants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                        >
-                        I am a passionate frontend developer with a love for creating
-                        beautiful and functional web experiences. My journey started with
-                        HTML and CSS, and now I explore React, animations, and more!
-                        </motion.p>
-                    </>
-                    )}
-                </AnimatePresence>
+                    <AnimatePresence>
+                        {isExpanded.container1 && (
+                        <>
+                            <motion.img
+                                src={about1}
+                                alt="About Me 1"
+                                style={{
+                                    width: '450px',
+                                    height: '400px',
+                                    objectFit: 'cover',
+                                    borderRadius: '10px',
+                                    margin: '20px 0',
+                                }}
+                                variants={imageVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="hidden"
+                            />
+                            <motion.p
+                                style={{
+                                    maxWidth: '600px',
+                                    margin: '20px auto',
+                                    fontSize: '29px',
+                                }}
+                                variants={textVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="hidden"
+                            >
+                                <strong>I am a passionate frontend developer with a love for creating
+                                beautiful and functional web experiences. My journey started with
+                                HTML and CSS, and now I explore React, animations, and more!</strong>
+                                
+                            </motion.p>
+                        </>
+                        )};
+                    </AnimatePresence>
                 </motion.div>
 
                 {/* Контейнер 2 */}
                 <motion.div
-                className="about-container"
-                initial={{ opacity: 0, y: -200, rotate: -10 }}
-                whileInView={{ opacity: 1, y: 0, rotate: 0 }}
-                viewport={{ once: true, amount: 0.3 }}
-                variants={containerVariants}
-                animate={isExpanded.container2 ? 'expanded' : 'collapsed'}
-                style={{
-                    marginBottom: '50px',
-                    textAlign: 'center',
-                    backgroundColor: '#6ad4ad',
-                    padding: '20px',
-                    borderRadius: '10px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    maxWidth: '1200px',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                }}
+                    className="about-container"
+                    initial={{ opacity: 0, y: -200, rotate: -10 }}
+                    whileInView={{ opacity: 1, y: 0, rotate: 0 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    variants={containerVariants}
+                    animate={isExpanded.container2 ? 'expanded' : 'collapsed'}
+                    style={{
+                        marginBottom: '50px',
+                        textAlign: 'center',
+                        backgroundColor: '#1ac9c9',
+                        padding: '20px',
+                        borderRadius: '10px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        maxWidth: '1200px',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                    }}
                 >
                 <motion.h2
                     className="title-1"
@@ -186,111 +197,112 @@ const containerVariants = {
                 >
                     My Skills
                 </motion.h2>
-                <AnimatePresence>
-                    {isExpanded.container2 && (
-                    <>
-                        <motion.img
-                        src={about2}
-                        alt="About Me 2"
-                        style={{
-                            width: '450px',
-                            height: '400px',
-                            objectFit: 'cover',
-                            borderRadius: '10px',
-                            margin: '20px 0',
-                        }}
-                        variants={imageVariants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                        />
-                        <motion.p
-                        style={{
-                            maxWidth: '600px',
-                            margin: '20px auto',
-                            fontSize: '25px',
-                        }}
-                        variants={textVariants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                        >
-                        I specialize in JavaScript, React, and modern CSS frameworks like
-                        TailwindCSS. I also have experience with backend technologies like
-                        Node.js and databases.
-                        </motion.p>
-                    </>
-                    )}
-                </AnimatePresence>
+                    <AnimatePresence>
+                        {isExpanded.container2 && (
+                        <>
+                            <motion.img
+                                src={about2}
+                                alt="About Me 2"
+                                style={{
+                                    width: '450px',
+                                    height: '400px',
+                                    objectFit: 'cover',
+                                    borderRadius: '10px',
+                                    margin: '20px 0',
+                                }}
+                                variants={imageVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="hidden"
+                            />
+                            <motion.p
+                                style={{
+                                    maxWidth: '600px',
+                                    margin: '20px auto',
+                                    fontSize: '29px',
+                                }}
+                                variants={textVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="hidden"
+                            >
+                                <strong>I specialize in JavaScript, React, and modern CSS frameworks like
+                                TailwindCSS. I also have experience with backend technologies like
+                                Node.js and databases.</strong>
+                            </motion.p>
+                        </>
+                        )}
+                    </AnimatePresence>
                 </motion.div>
 
                 {/* Контейнер 3 */}
                 <motion.div
-                className="about-container"
-                initial={{ opacity: 0, x: 100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                viewport={{ once: true, amount: 0.3 }}
-                variants={containerVariants}
-                animate={isExpanded.container3 ? 'expanded' : 'collapsed'}
-                style={{
-                    marginBottom: '50px',
-                    textAlign: 'center',
-                    backgroundColor: '#6ad4ad',
-                    padding: '20px',
-                    borderRadius: '10px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    maxWidth: '1200px',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                }}
+                    className="about-container"
+                    initial={{ opacity: 0, x: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    variants={containerVariants}
+                    animate={isExpanded.container3 ? 'expanded' : 'collapsed'}
+                    style={{
+                        marginBottom: '50px',
+                        textAlign: 'center',
+                        backgroundColor: '#f86c6c',
+                        padding: '20px',
+                        borderRadius: '10px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        maxWidth: '1200px',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                    }}
                 >
-                <motion.h2
-                    className="title-1"
-                    onClick={() => handleTitleClick('container3')}
-                    style={{ cursor: 'pointer' }}
-                    variants={titleVariants}
-                    animate={isExpanded.container3 ? 'shifted' : 'initial'}
-                >
-                    My Journey
-                </motion.h2>
-                <AnimatePresence>
-                    {isExpanded.container3 && (
-                    <>
-                        <motion.img
-                        src={about3}
-                        alt="About Me 3"
-                        style={{
-                            width: '450px',
-                            height: '400px',
-                            objectFit: 'cover',
-                            borderRadius: '10px',
-                            margin: '20px 0',
-                        }}
-                        variants={imageVariants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                        />
-                        <motion.p
-                        style={{
-                            maxWidth: '600px',
-                            margin: '20px auto',
-                            fontSize: '25px',
-                        }}
-                        variants={textVariants}
-                        initial="hidden"
-                        animate="visible"
-                        exit="hidden"
-                        >
-                        From learning basics to building complex projects, my journey has
-                        been filled with challenges and growth. I’m excited to continue
-                        learning and collaborating!
-                        </motion.p>
-                    </>
-                    )}
-                </AnimatePresence>
+                    <motion.h2
+                        className="title-1"
+                        onClick={() => handleTitleClick('container3')}
+                        style={{ cursor: 'pointer' }}
+                        variants={titleVariants}
+                        animate={isExpanded.container3 ? 'shifted' : 'initial'}
+                    >
+                        My Journey
+                    </motion.h2>
+                        <AnimatePresence>
+                            {isExpanded.container3 && (
+                            <>
+                                <motion.img
+                                src={about3}
+                                alt="About Me 3"
+                                style={{
+                                    width: '450px',
+                                    height: '400px',
+                                    objectFit: 'cover',
+                                    borderRadius: '10px',
+                                    margin: '20px 0',
+                                }}
+                                variants={imageVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="hidden"
+                                />
+                                <motion.p
+                                style={{
+                                    maxWidth: '600px',
+                                    margin: '20px auto',
+                                    fontSize: '29px',
+                                }}
+                                variants={textVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="hidden"
+                                >
+                                    <strong>From learning basics to building complex projects, my journey has
+                                been filled with challenges and growth. I’m excited to continue
+                                learning and collaborating!</strong>
+                                
+                                </motion.p>
+                            </>
+                        )}
+                    </AnimatePresence>
                 </motion.div>
             </div>
         </main>
